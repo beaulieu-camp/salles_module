d9b733e520695ad685c14f822f6bb59e74982fbd
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-01-10 18:47:24 +0100

    update majeur

--- tests/index.js
+++ tests/index.js
@@ -33,12 +33,18 @@ let server = app.listen(5000);
     console.log(val)
     
     esir.salleLibres(Object.keys(salles) , (results) => { 
-      console.log( results ) 
-    }, Date.now())
+      console.log( results["test1"]["state"] === false ) 
+      console.log( results["test1"]["until"] === 1673450100000 ) 
+      console.log( results["debug"]["test1"][0] ) 
+      console.log( results["debug"]["test1"][1] ) 
+      console.log( results["date"] ) 
+    }, 1673447400000)
 
     esir.salleEvents(Object.keys(salles) , (results) => { 
-      console.log( results["test1"][0] ) 
-    }, Date.now())
+      console.log( results["test1"][0]["uid"] === "ADE60456d706c6f6973647574656d7073323032322d323032332d383536352d312d30" ) 
+      console.log( results["test1"][1]["uid"] === "ADE60456d706c6f6973647574656d7073323032322d323032332d35373335312d302d30" ) 
+      console.log( results["test1"][2]["uid"] === "ADE60456d706c6f6973647574656d7073323032322d323032332d38363232302d302d30" ) 
+    }, 1673447400000)
 
     // fetch("http://localhost:5000/close")
   })
